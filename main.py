ed14df4796fd7c6bf8e43817c383c3f5b4b562bb
MizushimaKeita <mizushima.keita@lancers.co.jp>
2019-03-18 18:02:55 +0900

    並列処理を追加して、エラーを出にくくする

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 import json
+import threading
+
+from post_message_to_slack import post_message_to_slack, post_personal_message_to_slack
+
+#  並列処理させてエラーを出にくくする
+def lambda_function(event, content):
+    thread_1 = threading.Thread(target=return_200)
+    thread_2 = threading.Thread(target=main_func(event, content))
+    return 0
+
+# slackにとりあえず200を返す
+def return_200():
+    print("prodess kill")
+    return 0
+
 
 def lambda_handler(event, context):
     # slackからの投稿を slack_input_text へ格納
@@ -13,8 +28,6 @@ def lambda_handler(event, context):
         action = event['action']
         PR = event['pull_request']
 
-        # print(PR.keys())
-        # print(PR['merged'])
         # PRがmergeされた時
         if (action == 'edited' and PR['merged']):
             print('関数がマージされたよ。')
