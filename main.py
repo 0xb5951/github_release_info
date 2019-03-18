f171557817e400ba6a076e12e3008abc7e333d3f
MizushimaKeita <mizushima.keita@lancers.co.jp>
2019-03-18 17:28:44 +0900

    PullRequestがマージされた時の判断基準を追加

--- main.py
+++ main.py
@@ -4,12 +4,20 @@ def lambda_handler(event, context):
     # slackからの投稿を slack_input_text へ格納
     print("関数呼び出されsection")
     # print(type(event))
-    # print(event.keys())
+    print(event.keys())
     print(event)
 
     # EventがPullRequestの時
     if ('pull_request' in event):
         print('PullRequestEventから呼ばれたよ！')
+        action = event['action']
+        PR = event['pull_request']
+
+        # print(PR.keys())
+        # print(PR['merged'])
+        # PRがmergeされた時
+        if (action == 'edited' and PR['merged']):
+            print('関数がマージされたよ。')
 
     # EventがPushsの時
     elif ('ref' in event):
